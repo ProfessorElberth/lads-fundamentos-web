6facc07853a89c6e2f2e6ed019f27b3b2646e3c0
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-04-23 08:33:39 -0300

    Criacao de novas variaveis e calculos

--- Primeiro.java
+++ Primeiro.java
@@ -1,12 +1,28 @@
 public class Primeiro{
-	//%s: texto; %f: real; %d: inteiro
+
 	public static void main(String[] args)	{		
-		System.out.println("Nome: " + args[0]);
-		System.out.printf("Nome: %s\n", args[0]);
-		System.out.println(args[1]);
-		System.out.println(args[2]);
-		System.out.println(args[3]);
-		System.out.println(args[4]);
-	}
+	
+		if(args.length == 6){
+			String nome = args[0];						
+			String sobrenome = args[1];					
+			int idade = Integer.valueOf(args[2]);		
+			float salario = Float.valueOf(args[3]);					
+			int qtdeMeses = Integer.valueOf(args[5]);
+			boolean ehJava = "S".equalsIgnoreCase(args[4]) ? true : false;
+			
+			int anoNascimento = 2021 - idade;
+			float salarioTotal = salario * qtdeMeses;
+
+			System.out.println("Nome: " + nome);
+			System.out.println("Sobrenome: " + sobrenome);
+			System.out.println("Idade: " + idade);
+			System.out.println("Nascimento: " + anoNascimento);
+			System.out.println("Salario: " + salario);
+			System.out.println("Java? " + ehJava);
+			System.out.println("Salario Total: " + salarioTotal);
+		} else {		
+			System.out.println("Problemas na quantidade de parametros.");
+		}
+	}	
 	
 }
