b7d0246b78a394a0e832e9aebbe74d1d0b8fb199
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-04-22 22:32:36 -0300

    Criacao da classe, recebimento de argumentos e exibicao das informacoes

---
+++ Primeiro.java
@@ -0,0 +1,12 @@
+public class Primeiro{
+	//%s: texto; %f: real; %d: inteiro
+	public static void main(String[] args)	{		
+		System.out.println("Nome: " + args[0]);
+		System.out.printf("Nome: %s\n", args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[3]);
+		System.out.println(args[4]);
+	}
+	
+}
