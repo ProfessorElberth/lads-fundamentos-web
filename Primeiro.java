0b142ffe92251c50ad594b1e804af36480684dfd
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-04-28 08:37:43 -0300

    Condicionais e metodos, parametros

--- Primeiro.java
+++ Primeiro.java
@@ -1,17 +1,38 @@
 public class Primeiro{
 
+	private static String[] parametros;
+
+	private static int idade;
+	private static float salario;
+	private static String nome;
+	private static String sobrenome;
+	private static int qtdeMeses;
+	private static boolean ehJava;
+	
+	private static void tratarParametros(){
+		nome = parametros[0];
+		sobrenome = parametros[1];
+		idade = Integer.valueOf(parametros[2]);
+		salario = Float.valueOf(parametros[3]);
+		qtdeMeses = Integer.valueOf(parametros[5]);
+		ehJava = "S".equalsIgnoreCase(parametros[4]) ? true : false;
+	}
+	
+	private static boolean validar(int qtde){		
+		return qtde == 6;
+	}
+
 	public static void main(String[] args)	{		
 	
-		if(args.length == 6){
-			String nome = args[0];						
-			String sobrenome = args[1];					
-			int idade = Integer.valueOf(args[2]);		
-			float salario = Float.valueOf(args[3]);					
-			int qtdeMeses = Integer.valueOf(args[5]);
-			boolean ehJava = "S".equalsIgnoreCase(args[4]) ? true : false;
-			
+		if(validar(args.length)){
+			parametros = args;
+
+			tratarParametros();
+
 			int anoNascimento = 2021 - idade;
 			float salarioTotal = salario * qtdeMeses;
+			String nivel = idade < 50 ? "iniciante" : "veterano";
+			String situacao = salarioTotal > 1000 ? "estavel" : "instavel";
 
 			System.out.println("Nome: " + nome);
 			System.out.println("Sobrenome: " + sobrenome);
@@ -20,9 +41,10 @@ public class Primeiro{
 			System.out.println("Salario: " + salario);
 			System.out.println("Java? " + ehJava);
 			System.out.println("Salario Total: " + salarioTotal);
+			System.out.println("Nivel: " + nivel);
+			System.out.println("Situacao: " + situacao);
 		} else {		
 			System.out.println("Problemas na quantidade de parametros.");
 		}
-	}	
-	
+	}		
 }
